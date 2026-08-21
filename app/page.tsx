19da5f668ac0a2c4ccff6723d0ef2918e92d7075
Talha <pertubuhanliterasitanah@gmail.com>
2026-08-21 09:56:57 +0800

    Tambah section Sertai Pertubuhan dengan banner baru, buang MembershipCTA kecil

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ export const dynamic = "force-dynamic";
 import Hero from "@/components/Hero";
 import FeatureCards from "@/components/FeatureCards";
 import ClassTable from "@/components/ClassTable";
-import MembershipCTA from "@/components/MembershipCTA";
 import ProductGrid from "@/components/ProductGrid";
 import ActivityGrid from "@/components/ActivityGrid";
 import AdCarousel from "@/components/AdCarousel";
+import SertaiKami from "@/components/SertaiKami";
 import { prisma } from "@/lib/prisma";
 
 export default async function HomePage() {
@@ -20,18 +20,14 @@ export default async function HomePage() {
       <Hero />
       <FeatureCards />
 
-      <div className="max-w-7xl mx-auto px-6 pt-16 grid lg:grid-cols-3 gap-8 items-start">
-        <div className="lg:col-span-2">
-          <ClassTable />
-        </div>
-        <div className="lg:mt-16">
-          <MembershipCTA />
-        </div>
+      <div className="max-w-7xl mx-auto px-6 pt-16">
+        <ClassTable />
       </div>
 
       <ProductGrid />
       <ActivityGrid />
       <AdCarousel ads={ads} />
+      <SertaiKami />
     </>
   );
 }
